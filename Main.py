b12ddb72bc9303339927c98931e397237498e6a3
Jack <jack.hare@hotmail.com>
2025-05-13 13:19:49 -0500

    Sample window

---
+++ Main.py
@@ -0,0 +1,15 @@
+import pygame
+
+background_colour = (255,255,255)
+(width, height) = (1280, 720)
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Minecraft2d")
+screen.fill(background_colour)
+pygame.display.flip()
+
+
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
